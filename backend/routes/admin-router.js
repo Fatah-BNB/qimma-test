c32cbdb4341187f2a5a73d579c6c36d74979b8ea
Meauh <mohamed.lalouani@univ-constantine2.dz>
2023-04-04 12:39:52 +0100

    create first admin feature (count users) Add: countUsers function in services, admin-count route Update: link the service with its controller.

--- backend/routes/admin-router.js
+++ backend/routes/admin-router.js
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post('/login', adminController.loginController);
 router.post('/logout', adminController.logoutController);
 router.get('/users', checkToken.verifyToken, adminController.getAllUsersController);
+router.post('/admin-count', adminController.countUsersController);
 
 module.exports = router;
