813b2d90d89fdbb4a5345687143e62ee67a1ac53
Meauh <mohamed.lalouani@univ-constantine2.dz>
2023-04-13 04:21:31 +0100

    Password resset fixed Update: Change front route, update password reset page, Fix login,  update sent URL

--- backend/controllers/user-auth-controller.js
+++ backend/controllers/user-auth-controller.js
@@ -105,7 +105,7 @@ function passwordResettingCntrl(req, res) {
     try {
       //1) create token for email 
       const emailToken = userService.createEmailToken(results[0].user_firstName, results[0].user_id);
-      var fullUrl = req.protocol + '://' + req.get('host') + '/login/password-resetting/'+ results[0].user_firstName + '/' + emailToken
+      var fullUrl = req.protocol + '://' + "localhost:3000" + '/password-reset/'+ results[0].user_firstName + '/' + emailToken
 
       //2) send the email
       userService.sendEmail(results[0].user_email, fullUrl, 'Password resetting');
